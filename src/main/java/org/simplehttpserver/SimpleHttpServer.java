3a9fa3b5021b0e3a9db84914f7d587153cac440a
ilis137 <iliyasshahapure1@gmail.com>
2023-01-10 15:42:07 +0530

    initial commit

---
+++ src/main/java/org/simplehttpserver/SimpleHttpServer.java
@@ -0,0 +1,15 @@
+package org.simplehttpserver;
+
+/**
+ * Hello world!
+ *
+ */
+public class SimpleHttpServer
+{
+    public static final int DEFAULT_PORT=8080;
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello World!" );
+    }
+}
