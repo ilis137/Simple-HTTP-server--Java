04ab152508895acc02a1449f426145779bee352f
ilis137 <iliyasshahapure1@gmail.com>
2023-01-10 16:20:04 +0530

    basic socket

--- src/main/java/org/simplehttpserver/SimpleHttpServer.java
+++ src/main/java/org/simplehttpserver/SimpleHttpServer.java
@@ -1,15 +1,34 @@
 package org.simplehttpserver;
 
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
 /**
  * Hello world!
- *
  */
-public class SimpleHttpServer
-{
-    public static final int DEFAULT_PORT=8080;
-    public static void main( String[] args )
-    {
+public class SimpleHttpServer {
 
-        System.out.println( "Hello World!" );
+    private HttpServer httpServer;
+    private int port;
+
+
+    private void start(int port) {
+        this.port = port;
+        try {
+            httpServer = HttpServer.create(new InetSocketAddress(port), 0);
+            System.out.println("server started at port " + port);
+            httpServer.setExecutor(null);
+            httpServer.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        final int DEFAULT_PORT = 8080;
+        SimpleHttpServer simpleHttpServer = new SimpleHttpServer();
+        simpleHttpServer.start(DEFAULT_PORT);
     }
 }
